f905dd27a1efbe37a501fbbfe26cce6a066cc079
yoojinlee-ai <yoojinlee@sookmyung.ac.kr>
2025-11-27 15:27:52 +0900

    13-2

--- main.c
+++ main.c
@@ -1,27 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>  
+#include <math.h>
 
-// 구조체 정의
-struct student {
-    int ID;           // 학번
-    char name[20];    // 이름
-    double grade;     // 학점
+struct point {
+    int x;
+    int y;
 };
 
-int main(int argc, char *argv[]) {
-    // 변수 선언 및 초기화
-    struct student stud = {10, "es", 4.3};
+int main(void) {
+    struct point p1, p2;
+    int xdiff, ydiff;
+    double dist;
 
-    // 학번, 이름, 학점 값 변경
-    stud.ID = 17;
-    strcpy(stud.name, "song");
-    stud.grade = 2.0;
+    printf("input p1 coordinate (x y) : ");
+    scanf("%d %d", &p1.x, &p1.y);
 
-    // 각각 출력
-    printf("ID: %d, name: %s, grade: %.1f\n", stud.ID, stud.name, stud.grade);
+    printf("input p2 coordinate (x y) : ");
+    scanf("%d %d", &p2.x, &p2.y);
 
-    system("pause");
+    xdiff = p2.x - p1.x;
+    ydiff = p2.y - p1.y;
+
+    dist = sqrt((double)(xdiff * xdiff + ydiff * ydiff));
+
+    printf("distance : %f\n", dist);
+
     return 0;
 }
 
